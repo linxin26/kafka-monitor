dd296ee93fb8f1d75aa88b4e96f1fe5ff78ccf86
linxin26 <644299146@qq.com>
2019-07-26 10:49:50 +0800

    清除垃圾引用。

--- src/main/java/co/solinx/kafka/monitor/config/PreConfig.java
+++ src/main/java/co/solinx/kafka/monitor/config/PreConfig.java
@@ -1,7 +1,6 @@
 package co.solinx.kafka.monitor.config;
 
 import co.solinx.kafka.monitor.api.InitBean;
-import com.sun.org.apache.xml.internal.security.Init;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
